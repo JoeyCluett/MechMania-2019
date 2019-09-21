778ec6db1ea07d4df9129de2f365e058c1c60c40
sirdude103 <sirdude103@gmail.com>
2019-09-20 20:35:40 -0500

    Initial clone of MechMania repo

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# MechMania-2019
+# MechMania25 Python Starter Pack
+
+Here's all the code you need to get started with making a bot for MechMania in Python. Just do these steps:
+
+* Pre-Setup -- install Java, Node, and the `mm` command line tools
+* Setup -- Clone this repository and start running your bot!
+
+# Pre-Setup
+1. Follow the Pre-setup instructions on the wiki [here](https://github.com/HoelzelJon/MechMania-25-Wiki/wiki#pre-setup)
+
+2. Now install Python 3. To do this, see [this guide](https://realpython.com/installing-python/) for help.
+
+3. For the python starter pack, you will have to set up pip
+    * To set up pip, you can follow [this guide](https://www.makeuseof.com/tag/install-pip-for-python/) .
+    * You will also need [Flask](https://pypi.org/project/Flask/), which you can get via the command line by running `pip install flask` (or `pip3 install flask` if you have pip3)
+    * If you get a `no module named [insert module name here]` error while running your scripts, you can run `pip install [insert module name here]`
+
+# Setup
+
+1. Clone this repo (or fork it or download it somewhere as a ZIP)
+2. Modify the script at `Strategy.py`.
+    * Write your code in the `do_turn` method and `get_setup` method.
+    * You may also add other files or dependencies. If you have any questions about this, we're here to help!
+
+4. Run `mm play .`
+    * This will build the bot in the given directory (`.`) and then starts a game in which your bot fights against itself.
+5. To run two different bots against each other, run `mm play bot1_directory bot2_directory`.
+6. To submit your bot, run `mm push .`
+
+Use `mm help` for more help or `mm play` for information about the different options/flags you can use while running a game!
+
+# What's this `ImportError: No module named queue`?
+
+If you're getting this error, you most likely have python 2 running instead of python 3. To fix this, change `python` to `python3` in the `mm.json` file.
+
+# Game API Information
+You will be writing your code inside of the `Strategy.py` file. Note, that the `Strategy` class inherits the `Game` class from `API.py`, so all the instance methods for a `Game` object are available to you. You can find documentation for these methods in [API.py](https://github.com/HoelzelJon/MM25-Python-Starter-Pack/blob/master/API.py).
---
+++ Strategy.py
@@ -0,0 +1,56 @@
+from API import Game
+
+
+class Strategy(Game):
+    """
+        FILL THIS METHOD OUT FOR YOUR BOT:
+        Method to set unit initializations. Run at the beginning of a game, after assigning player numbers.
+        We have given you a default implementation for this method.
+        OUTPUT:
+            An array of 3 dictionaries, where each dictionary details a unit. The dictionaries should have the following fields
+                "health": An integer indicating the desired health for that unit
+                "speed": An integer indicating the desired speed for that unit
+                "unitId": An integer indicating the desired id for that unit. In this provided example, we assign Ids 1,2,3 if you are player1, or 4,5,6 if you are player2
+                "attackPattern": a 7x7 2d integer list indicating the desired attack pattern for that unit
+                "terrainPattern": a 7x7 2d boolean list indicating the desired terrain pattern for that unit.
+        Note: terrainPattern and attackPattern should be indexed x,y. with (0,0) being the bottom left
+        If player_id is 1, UnitIds for the bots should be 1,2,3. If player_id is 2, UnitIds should be 4,5,6
+    """
+    def get_setup(self):
+        units = []
+        for i in range(3):
+            unit = {"health": 5, "speed": 5}
+            unit["attackPattern"] = [[0] * 7 for j in range(7)]
+            # if you are player1, unitIds will be 1,2,3. If you are player2, they will be 4,5,6
+            unit["unitId"] = i + 1
+            if self.player_id == 2:
+                unit["unitId"] += 3
+            unit["terrainPattern"] = [[False]*7 for j in range(7)]
+            # These sample bot will do damage to the tiles to its left, right, and up. And build terrain behind it
+            unit["attackPattern"][3][2] = 2
+            unit["attackPattern"][2][3] = 2
+            unit["attackPattern"][4][2] = 2
+            unit["terrainPattern"][3][2] = True
+            units.append(unit)
+        return units
+
+    """
+        FILL THIS METHOD OUT FOR YOUR BOT:
+        Method to implement the competitors strategy in the next turn of the game.
+        We have given you a default implementation here.
+        OUTPUT:
+            A list of 3 dictionaries, each of which indicates what to do on a given turn with that specific unit. Each dictionary should have the following keys:
+                "unitId": The Id of the unit this dictionary will detail the action for
+                "movement": an array of directions ("UP", "DOWN", "LEFT", or "RIGHT") details how you want that unit to move on this turn
+                "attack": the direction in which to attack ("UP", "DOWN", "LEFT", or "RIGHT")
+                "priority": The bots move one at a time, so give the priority which you want them to act in (1,2, or 3)
+    """
+    def do_turn(self):
+        my_units = self.get_my_units()
+        decision = [{
+            "priority": i+1,
+            "movement": ["DOWN"]*my_units[i].speed,
+            "attack": "DOWN",
+            "unitId": my_units[i].id
+            } for i in range(len(my_units))]
+        return decision
